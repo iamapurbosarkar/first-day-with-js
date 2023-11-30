eb0025e453a25d959e832c3c05713e79c8f01891
Apurbo-Sarkar-41 <iamapurbo1@gmail.com>
2023-12-01 00:32:45 +0600

    my first day with javascript

---
+++ test.js
@@ -0,0 +1,404 @@
+// var iphonePrice = 80000;
+// var myBudget = 81000;
+// if (iphonePrice < myBudget){
+//     console.log('iphone kine fotani marbo');
+// }
+
+// var chickenPrice = 280;
+// var myMoney = 200;
+// if (chickenPrice < myMoney){
+//     console.log('Today i will eat chicken')
+// }
+// else{
+//     console.log('I will eat Potato')
+// }
+
+
+// var isGraduated = true;
+// var salary = 2500;
+// if(isGraduated == true && salary < 1000){
+//     console.log('Ami biye korbo');
+// }
+// else{
+//     console.log('Amr kopala biye nai');
+// }
+
+// console.log(5 == 6);
+
+// console.log(8 <= 10)
+
+
+// var isGraduated = true;
+// var mySalary = 2500;
+// var myCars = 1;
+// if(isGraduated == false || myCars >= 1){
+//     console.log('Ami biye korbo')
+// }
+// else{
+//     console.log('Amr kopale biye nai')
+// }
+
+//             //Practice// //13,79,45
+
+// var thirteen = 13;
+// var seventyNine = 79;
+// var fortyFive = 45;
+// if( thirteen > fortyFive){
+// }
+// else if ( fortyFive > seventyNine){
+
+// }
+// else if ( fortyFive < seventyNine){
+//     console.log('79 is the largest number')
+// }
+          //another system//
+
+// var myName = 'Apurbo Sarkar';
+// console.log(typeof myName);
+
+
+// var x = 25.456;
+// var y = parseInt(x);
+// console.log(y);
+
+// var num = '14.25';
+// var num1 = parseFloat(num);
+// console.log(parseInt(num1));
+
+// var x = 5;
+// console.log(x++);
+// console.log(x++);
+
+// var y = 10;
+// console.log(++y);
+// console.log(++y);
+
+// var avr = [10, 12, 14, 16];
+// console.log(avr.length);
+// console.log(avr[2]);
+
+// var mark = 69;
+// if(mark > 80){
+//     console.log('I got A+')
+// }
+// else if(mark > 70){
+//     console.log('I got A')
+// }
+// else if(mark > 60){
+//     console.log('I got B')
+// }
+// else if(mark > 50){
+//     console.log('I got C')
+// }
+// else{
+//     console.log('I am Fail')
+// }
+
+//    Temperature 
+
+// var temperature = 25;
+// var isRaining = true;
+// if(temperature > 25){
+//     console.log('It is hot summer day');
+//     if(isRaining == true){
+//         console.log('Today is Raining day')
+//     }
+//     else{
+//         console.log('Today is not Raining day')
+//     }
+// }
+// else{
+//     console.log('It is cold day');
+//     if(isRaining){
+//         console.log('but it is raining')
+//     }
+//     else{
+//         console.log('and it is not raining')
+//     }
+// }
+
+// var tableItems = ['Key', 'Mouse', 'Paper', 'Pen', 'Toothbrush'];
+// var items3 = tableItems[3];
+// tableItems[2] = 'Mobile';
+
+// var roastGiven = 1;
+// while(roastGiven <= 8){
+//     console.log('Roast Den Please')
+//     console.log(roastGiven)
+//     roastGiven += 1;
+// }
+
+// var classStudents = 15;
+// while(classStudents <= 20){
+//     console.log('Present Sir')
+//     console.log(classStudents)
+//     classStudents += 1;
+// }
+
+// var examination = 1;
+// while(examination <= 7){
+//     console.log('Exam Finished');
+//     examination += 1;
+// }
+
+// var number = 5;
+// while(number <= 20){
+//    console.log(number);
+//    number = number + 5;
+// }
+
+// for(var i =0; i <= 20; i+= 2){
+//    console.log(i);
+// }
+
+// for(var i = 1; i <= 5; i+= 1){
+//    console.log('Ami tomake valo basi')
+// }
+
+// var numbers = [10, 13, 44, 66, 12, 14, 27, 30, 55, 56, 57, 58, 90, 40, 70];
+// for(var i = 0; i <= numbers.length; i+= 1){
+//    var number = numbers[i];
+//    console.log(number);
+// }
+
+// var numbers = [33, 44, 55, 77, 88, 11, 22, 33, 43, 88, 999];
+// for(var i = 0; i <= numbers.length; i+= 1){
+//    var number = numbers[i];
+//    console.log(number);
+// }
+//                 //Think one this//
+
+// for(var i = 1; i <= 20; i++){
+//    if(i > 5){
+//       break;
+//    }console.log(i);
+// }
+
+// for(var i = 1; i <= 50; i++){
+//    if(i > 10){
+//       break;
+//    }console.log(i);
+// }
+// for(var i = 1; i <= 50; i++){
+//    if(i > 30){
+//       break;
+//    }console.log(i);
+// }
+
+// var numbers = [33, 32, 44, 55, 2344, 45, 66, 88, 12, 15, 44];
+// for(var i = 0; i < numbers.length; i++){
+//    var number = numbers[i];
+//    if(number > 50){
+//       continue
+//    }console.log(number);
+// }
+
+// var num = 15;
+// while(num > 1){
+//    num--;
+//    console.log(num);
+// }
+
+// for(var i = 8; i >= 1; i--){
+//    console.log(i);
+// }
+
+
+// for(var i = 10; i >= 1; i--){
+//    console.log(i);
+// }
+
+// for(var i = 1; i <= 10; i++){
+//    console.log(i);
+// }
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var bananaIndex = fruits.indexOf('Banana');
+// fruits [bananaIndex] = 'Mango';
+// fruits.pop('Orange');
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// var bananaIndex = fruits.indexOf('Banana');
+// fruits [bananaIndex] = 'Mango';
+// var orangeIndex = fruits.indexOf('Orange');
+// fruits.pop(orangeIndex);
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+// var myMarks = 85;
+// var tomMarks = 66;
+// var janeMarks = 95;
+// var peterMarks = 56;
+// var johnMarks = 40;
+// if(aGrade <= 80){
+//    console.log('You got A');
+// }
+// else if(bGrade <= 60){
+//    console.log('You got B')
+// }
+// else if(cGrade <= 50){
+//    console.log('You got C')
+// }
+// else if(dGrade <= 40){
+//    console.log('You got D')
+// }
+// else if(fGrade >= 39){
+//    console.log('You got F')
+// }
+// console.log(myMarks);
+
+// var numbers = [33, 32, 44, 55, 2344, 45, 66, 88, 12, 15, 44];
+// for(var i = 0; i < numbers.length; i++){
+//    var number = numbers[i];
+//    if(number > 50){
+//       continue
+//    }console.log(number);
+// }
+
+// var myMarks = 85;
+// var tomMarks = 66;
+// var janeMarks = 95;
+// var peterMarks = 56;
+// var johnMarks = 40;
+
+// var aGrade = 80;
+// var bGrade = 60;
+// var cGrade = 50;
+// var dGrade = 40;
+// var fGrade = 39;
+
+// var numbers = [85, 66, 95, 56, 40];
+// var myMarks = 85;
+// var tomMarks = numbers.indexOf(66);
+// var janeMarks = numbers.indexOf(95);
+// var peterMarks = numbers.indexOf(56);
+// var janeMarks = numbers.indexOf(40);
+// if(numbers <= 80){
+//    console.log('You got A');
+// }
+// else if(numbers <= 60){
+//    console.log('You got B')
+// }
+// else if(numbers <= 50){
+//    console.log('You got C')
+// }
+// else if(numbers <= 40){
+//    console.log('You got D')
+// }
+// else if(numbers >= 39){
+//    console.log('You got F')
+// }
+// console.log(tomMarks);
+
+
+//  var num1 = 13;
+//  var num2 = 79;
+//  var num3 = 145;
+// if(num1 > num2){
+//    if(num1 > num3){
+//       console.log(num1);
+//    }else{
+//       console.log(num3);
+//    }
+// }
+// else{
+//    if(num2 > num3){
+//       console.log(num2);
+//    }else{
+//       console.log(num3);
+//    }
+// }
+
+// var num1 = 15;
+// var num2 = 55;
+// var num3 = 88;
+// if(num1 > num2){
+//    if(num1 > num3);
+//    console.log(num1);
+// }
+// else{
+//    if(num2 > num3){
+//       console.log(num2);
+//    }else{
+//       console.log(num3);
+//    }
+// }
+
+
+            //Finally Success//
+
+// var num1 = 33;
+// var num2 = 44;
+// var num3 = 55;
+// var num4 = 66;
+// if(num1 > num2 || num1 > num3 || num1 > num4){
+//    console.log(num1);
+// }else{
+//    if(num2 > num3 || num2 > num4){
+//       console.log(num2);
+//    }else{
+//       if(num3 > num4){
+//          console.log(num3);
+//       }else{
+//          console.log(num4);
+//       }
+//    }
+// }
+
+            //Display numbers between 58 to 98//
+// for(i = 58; i <= 98; i++){
+//    console.log(i);
+// }
+
+            //Show all even numbers(jor sonkha) from 412 to 456//
+
+
+// for(i = 412; i <= 456; i+= 2){
+//    console.log(i);
+// }
+ 
+            //Show all Odd Numbers 581 to 623//
+
+// for(i = 581; i <= 623; i+= 2){
+//    console.log(i);
+// }
+
+         //Display ''Ajke amr Mon Valo Nei for 15 times''//
+
+
+// for(i = 1; i <= 15; i++){
+//    console.log('Ajke Amr Mon Valo Nei');
+// }
+
+
+  //Write the price of the books that you have.Display the prices if the prices is lower than 200//
+
+// var book1Price = 400;
+// var book2Price = 200;
+// var book3Price = 28;
+// if(book1Price < book2Price || book1Price < book3Price){
+//    console.log('book-1 is low Prices');
+// }else{
+//    if(book2Price < book3Price){
+//       console.log('Book-2 is low Price');
+//    }else{
+//       console.log('book-3 is low Prices');
+//    }
+// }
+         //Using Slice//
+// var numbers = [44, 55, 66, 22, 33, 88, 23, 56, 99];
+// var numOf = numbers.slice(1, 6);
+// console.log(numOf);
+  //includes means that numbers er moddhe ace ki na eta true or false er madhome sure kore//
+
+// var includesOf = numbers.includes(44);
+// console.log(includesOf);
+
+      //Substring//
+
+var fabSinger = 'My Favorite Singer is Arijit Sing';
+var favoriteOne = fabSinger.substring(10, 18);
+console.log(favoriteOne);
+
